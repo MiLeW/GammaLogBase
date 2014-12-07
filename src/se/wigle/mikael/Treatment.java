91134c5063ccdfe2b79c44a398eec89ad29ef958
Mikael Wigle <mikael@wigle.se>
2014-12-07 11:42:37 +0100

    Updates

--- src/se/wigle/mikael/Treatment.java
+++ src/se/wigle/mikael/Treatment.java
@@ -1,5 +1,9 @@
 package se.wigle.mikael;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 /**
  * Created by mikae_000 on 2014-11-20.
  */
@@ -7,6 +11,7 @@ public class Treatment {
 	public void addDose(Dose newDose)
 	{
 	}
-	
-	
+
+	private Date mDate;
+	private List<Dose> lDoses = new ArrayList<Dose>();
 }
